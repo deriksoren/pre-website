b45c33302be95157a9cd56790437d1656ad6c14e
Erik Sorensen <deriksoren@gmail.com>
2023-04-01 20:07:21 -0400

    Add image and p tags. Change colors.

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Actor. Programer. Shakespearean.";
 document.querySelector("body").appendChild(h2);
+
+const img = document.createElement ("img");
+img.src = "onStage.jpg";
+document.querySelector("body").appendChild(img);
+
+
+const p1 = document.createElement("p1");
+p1.textContent = "I'm a lover of language who believes in the power of expressive theatre. I follow my creativity, and I am guided by the creativity around me. I am a classically trained Shakespearean actor, and I believe in the power of words. I'm excited to take my creative lens on my new adventure as a programmer.";
+document.querySelector("body").appendChild(p1);
